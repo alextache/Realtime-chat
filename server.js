d55a7d307ff8fd1bf505ab70ed8b3291de4a21a2
alextache <alex.tache61@gmail.com>
2021-01-20 21:26:25 +0000

    File changes

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
+const path = require('path');
+const http = require('http');
 const express = require ('express'); 
+const socketio = require('socket.io');
+
 const app = express(); 
+const server = http.createServer(app); 
+const io = socketio(server); 
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects 
+io.on('connection', socket => {
+
+    // Welcome current user
+    socket.emit('message', 'Welcome to ChatRoom');
+
+    // Broadcast when a user connects 
+    socket.broadcast.emit('message', 'A user has joined tha chat');
+    
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat'); 
+    }); 
+
+    // Lisen for message
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    })
+
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
