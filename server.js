0623005e0dba8cf54e7a8888f4be06f5441af764
alextache <alex.tache61@gmail.com>
2021-01-21 00:46:41 +0000

    File edits

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const path = require('path');
 const http = require('http');
 const express = require ('express'); 
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+
+// review this const
+const { format } = require('path');
 
 const app = express(); 
 const server = http.createServer(app); 
@@ -10,24 +15,39 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Chat Bot'; 
+
 // Run when a client connects 
 io.on('connection', socket => {
+    socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);
+        
+        socket.join(user.room);
 
-    // Welcome current user
-    socket.emit('message', 'Welcome to ChatRoom');
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatRoom!'));
 
-    // Broadcast when a user connects 
-    socket.broadcast.emit('message', 'A user has joined tha chat');
-    
-    // When a user disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat'); 
-    }); 
+        // Broadcast when a user connects 
+        socket.broadcast
+            .to(user.room)
+            .emit('message', formatMessage(botName, `${user.username} has joined tha chat`));
+    })
 
     // Lisen for message
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
     })
+
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socked.id); 
+
+        if(user){
+            io.to(user.room).emit('message', formatMessage(botName, `A user has left the chat`)); 
+        }
+    }); 
 
 });
 
